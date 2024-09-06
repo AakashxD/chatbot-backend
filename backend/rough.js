5ed9ee2fa348f86fcee4d8d8207c86f5fccba419
AakashxD <axkashsingh@gmail.com>
2024-09-06 17:43:20 +0530

    userInfo

---
+++ backend/rough.js
@@ -0,0 +1,7 @@
+// {
+//     "userName": "Aakash Singh",
+//     "mobileNumber": "9953276424",
+//     "museumsName": "GOAT DELHI",
+//     "ageCategory": "adult"
+    
+// }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express =require('express')
+const cors=require('cors')
 const {PORT} =require('./config/server.congif')
 const connect=require("./database/connection")
-const userRouter =require('./router/index')
+const userRouter =require('./router/user.routes')
 const app=express();
+app.use(cors());
 app.use(express.json()); 
 app.use('/api',userRouter)
 
